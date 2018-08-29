28c922d78cd29242076f1b0d3f1bf33e941fa1e5
cheuk.chan <cheukkwan_chan@outlook.com>
2018-08-29 19:00:08 -0400

    add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -31,6 +32,34 @@ app.get('/todos', (req, res) => {
         });
     }, (e) => {
         res.status(400).send(e);
+    })
+})
+
+// GET /todos/:ID
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    
+    //valid id using isValid
+        //stop and return 404 -send empty body
+    if(!ObjectID.isValid(id)){
+        res.status(400).send('invalid ID');
+    }
+    //findById
+        //success
+            //if todo- send it back
+            //if no todo - send back 404 with empty body
+        //error
+            //400 - send empty body
+
+    Todo.findById(id).then((result) => {
+        if(!result){
+            res.status(404).send();
+        }
+
+        res.send({ result });
+
+    }).catch((e) => {
+        res.status(400).send();
     })
 })
 
